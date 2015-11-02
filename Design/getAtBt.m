8d5bc4a2750c20ec9fec0ead91b7abfa87bba316
Fran <F.Bonilla@mobilewatermanagement.com>
2015-11-02 13:23:38 +0100

    function with comments

--- Design/getAtBt.m
+++ Design/getAtBt.m
@@ -17,4 +17,31 @@ Bt = [  -Bq*Df*Dp,          -Bq*Df;
         -Bf*Dp*Dp,          -Bf*Dp;
         -Bp*Dp,             -Bp;
          Bp,                 zeros(2,1)  ];
+     
+%%ALTERNATIVE CANONICAL FORM
+% Ct = ...
+%[Cq,         Dq*Cf,    zeros(1,4), zeros(1,1), zeros(1,2), -Dq*Df*Cp;
+% zeros(1,4), zeros(1,1), Cq,       Dq*Cf,     Dq*Df*Cp, Cp-Dq*Df*Dp*Cp];
+%
+% Dt = [ -Dq*Df*Dp,           -Dq*Df;
+%         Dp-Dq*Df*Dp*Dp,     -Dq*Df*Dp ];
+
+% THIS REPRESENTATION LEADS TO MATRIXES WHICH CONTAINS THE VARIABLE TO BE
+% OPTIMAZED, THEN THE COMPUTATION OF 'X' SHOULD BE INCLUDED IN THE
+% OPTIMIZATION FUNCTION.
+%
+%  At = ...
+%[ Af,         Bf*Cq,      zeros(1,2), zeros(1,1), zeros(1,4) -Bf*Dq*Cp;
+%  zeros(4,1), Aq,         zeros(4,2), zeros(4,1), zeros(4,4), -Bq*Cp;
+%  zeros(2,1), zeros(2,4), Ap,         Bp*Cf,      Bp*Df*Cq,-Bp*Df*Dp*Cp;
+%  zeros(1,1), zeros(1,4), zeros(1,2), Af,         Bf*Cq,     -Bf*Dq*Cp;
+%  zeros(4,1), zeros(4,4), zeros(4,2), zeros(4,1), Aq,        -Bq*Cp;
+%  zeros(2,1), zeros(2,4), zeros(2,2), zeros(2,1), zeros(2,4), Ap      ];
+% Bt = [  -Bf*Dq*Dp,          -Bf*Dq;
+%         -Bq*Dp,             -Bq;
+%         -Bp*Df*Dq*Dp,       -Bp*Df*Dq;
+%         -Bf*Dq*Dp,          -Bf*Dq;
+%         -Bq*Dp,             -Bq;
+%         Bp,                 zeros(2,1)  ];
+     
 end
